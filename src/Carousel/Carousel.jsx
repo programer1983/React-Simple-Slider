0c9f05fed0387d08c93c25d2f6d9965acecb3809
programer1983 <programer972@gmail.com>
2022-04-30 11:59:34 +0300

    commit-4;

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -1,11 +1,34 @@
 import "./Carousel.css"
+import {useState, useEffect, Children, cloneElement} from "react"
+
+const PAGE_WIDTH = 450
 
 const Carousel = ({children}) => {
+  const [pages, setPages] = useState([])
+
+
+  useEffect(() => {
+    setPages(
+        Children.map(children, (child) => {
+            return cloneElement(
+              child, {
+                style: {
+                    height: "100%",
+                    minWidth: `${PAGE_WIDTH}px`,
+                    maxWidth: `${PAGE_WIDTH}px`,
+                } 
+              }
+            )
+        })
+    )
+  }, [])
+
+
   return (
     <div className="main-container">
         <div className="window">
             <div className="all-pages-container">
-                {children}
+                {pages}
             </div>
         </div>
     </div>
